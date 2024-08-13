6ce4fd2965d535f150df254a597cbb6cfc111fe6
Mustafaelhdad <76115086+Mustafaelhdad@users.noreply.github.com>
2024-08-13 21:18:37 +0300

    login: show password and hide it

--- js/main.js
+++ js/main.js
@@ -78,3 +78,21 @@ loginForm.addEventListener("submit", function (event) {
 loginForm.addEventListener("input", function (event) {
   event.target.classList.remove("is-invalid");
 });
+
+document
+  .getElementById("togglePassword")
+  .addEventListener("click", function () {
+    const passwordInput = document.getElementById("password");
+    const icon = this;
+
+    // Toggle the type attribute
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      icon.classList.remove("fa-eye");
+      icon.classList.add("fa-eye-slash");
+    } else {
+      passwordInput.type = "password";
+      icon.classList.remove("fa-eye-slash");
+      icon.classList.add("fa-eye");
+    }
+  });
